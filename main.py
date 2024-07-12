7a356941e5d70a5d9bad6844b9d380b123f589b6
samilessons <samisecblog@gmail.com>
2024-07-12 13:23:38 +0400

    we created fake API which call the person, where we used dict, list & tuple and other types

---
+++ main.py
@@ -0,0 +1,31 @@
+
+person = {
+    "first_name": "John",
+    "last_name": "Smith",
+    "username": "johnsmith",
+    "email": "johnsmith@gmail.com",
+    "age": 21,
+    "isOnline": True,
+    "friends": ["Joe Gray", "Alex Sli", "Li Chu"],
+    "statuses": [
+        {
+            "id": 1,
+            "status": "Hi there, where i can buy original Nike",
+            "date": "08.07.2024",
+            "time": "14:50"
+        },
+        {
+            "id": 2,
+            "status": "I'm interested in medicine",
+            "date": "01.06.2024",
+            "time": "09:44"
+        },
+        {
+            "id": 3,
+            "status": "I'm interested in medicine",
+            "date": "01.06.2024",
+            "time": "09:44"
+        }
+    ],
+    "images": ("images/user1-1.webp", "images/user1-2.webp", "images/user1-3.webp")
+}
